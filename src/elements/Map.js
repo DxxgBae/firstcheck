b22774712488a03736fa2a0a663cbdd110c4167e
SonDongBae <dxxgbae@gmail.com>
2024-02-20 18:07:01 +0900

    집가자~

--- src/elements/Map.js
+++ src/elements/Map.js
@@ -6,12 +6,12 @@ import './Map.css';
 function Map() {
     const mapRef = useRef();
     const toolRef = useRef();
-    const keyVworld = 'A8901E28-B93C-3A14-B1C1-2FBC40EB22CA';
-    //const keyData = 'GXGoD02oAtHgVlYoMYAk%2FF4R7Z68cpmqauPMq9sw6L6lZfZWQfPzLsNZHMAs9P1ohYCffI%2BSxxD5iGwZtbwJKQ%3D%3D';
-    const { naver } = window;
     const { addFeature, removeFeature } = stateFeatures();
 
     useEffect(() => {
+        const { naver } = window;
+        const keyVworld = 'A8901E28-B93C-3A14-B1C1-2FBC40EB22CA';
+        //const keyData = 'GXGoD02oAtHgVlYoMYAk%2FF4R7Z68cpmqauPMq9sw6L6lZfZWQfPzLsNZHMAs9P1ohYCffI%2BSxxD5iGwZtbwJKQ%3D%3D';
         if (!mapRef.current || !naver) return;
 
         const map = new naver.maps.Map(mapRef.current, {
@@ -20,77 +20,60 @@ function Map() {
         });
         const cadastralLayer = new naver.maps.CadastralLayer();
 
-        const selectSite = (e) => {
-            fetchJsonp(`https://api.vworld.kr/req/data?request=GetFeature&key=${keyVworld}&data=LP_PA_CBND_BUBUN&crs=EPSG:4326&geomFilter=POINT(${e.coord._lng} ${e.coord._lat})`)
+        const selectSite = (e) =>
+            fetchJsonp(`https://api.vworld.kr/req/wfs?key=${keyVworld}&request=GetFeature&output=text/javascript&typename=lt_c_landinfobasemap&srsname=EPSG:4326&bbox=${e.coord._lng},${e.coord._lat},${e.coord._lng},${e.coord._lat}`)
                 .then(response => response.json())
-                .then(data => {
-                    if (!data.response.result) return;
-                    const featureCollection = data.response.result.featureCollection;
-                    featureCollection.bbox = null;
-                    map.data.addGeoJson(featureCollection);
+                .then(data => { })
+                .catch(error => { });
 
-                    const features = map.data.getAllFeature();
-                    const feature = features.at(-1);
-                    const pnu = feature.property_pnu;
-                    feature.marker = new naver.maps.Marker({
-                        position: new naver.maps.LatLng(feature.getBounds().getCenter()),
-                        map: map,
-                        icon: {
-                            content: `<div class="marker">${features.length}</div>`,
-                            anchor: new naver.maps.Point(10, 10),
-                        }
-                    });
-                    feature.setStyle({
-                        strokeLineCap: 'round',
-                        strokeLineJoin: 'round',
-                    });
-                    naver.maps.Event.addListener(feature, 'click', (e) => {
-                        e.feature.marker.setMap(null);
-                        map.data.removeFeature(e.feature);
-                        removeFeature(e.feature);
-                    });
+        window.parseResponse = function (featureCollection) {
+            const features = map.data.getAllFeature();
+            const pnu = featureCollection.features[0].properties.pnu;
+            for (var i of features) if (pnu === i.property_pnu) return;
+            featureCollection.bbox = null;
+            map.data.addGeoJson(featureCollection);
+
+            const feature = features.at(-1);
+            feature.marker = new naver.maps.Marker({
+                position: new naver.maps.LatLng(feature.getBounds().getCenter()),
+                map: map,
+                icon: {
+                    content: `<div class="marker">${features.length}</div>`,
+                    anchor: new naver.maps.Point(10, 10),
+                }
+            });
+            feature.setStyle({
+                strokeLineCap: 'round',
+                strokeLineJoin: 'round',
+            });
+            naver.maps.Event.addListener(feature, 'click', (e) => {
+                e.feature.marker.setMap(null);
+                map.data.removeFeature(e.feature);
+                removeFeature(e.feature);
+            });
+
+            const promises = [];
+            const fetchJsonpPromise = (url) => {
+                return fetchJsonp(url)
+                    .then(response => response.json())
+                    .catch(error => console.error(error));
+            };
+            promises.push(fetchJsonpPromise(`https://api.vworld.kr/ned/data/getPossessionAttr?key=${keyVworld}&pnu=${pnu}&numOfRows=1`));
+            promises.push(fetchJsonpPromise(`https://api.vworld.kr/ned/data/getLandUseAttr?key=${keyVworld}&pnu=${pnu}&numOfRows=100`));
+            promises.push(fetchJsonpPromise(`https://api.vworld.kr/ned/data/getIndvdLandPriceAttr?key=${keyVworld}&pnu=${pnu}&numOfRows=100`));
+            Promise.all(promises)
+                .then(responses => {
+                    const [possessionAttr, landUseAttr, indvdLandPriceAttr] = responses;
+
+                    if (possessionAttr.possessions.field[0]) feature.property_owner_count = Number(possessionAttr.possessions.field[0].cnrsPsnCo) + 1;
+                    if (landUseAttr.landUses.field.length > 0) feature.property_landUse = landUseAttr.landUses.field;
+                    if (indvdLandPriceAttr.indvdLandPrices.field.length > 0) feature.property_jiga = indvdLandPriceAttr.indvdLandPrices.field.reverse();
+
+                    feature.property_addr = [feature.property_sido_nm, feature.property_sgg_nm, feature.property_emd_nm, feature.property_ri_nm, feature.property_jibun].join(' ');
                     console.log(feature);
-                    console.log(feature.marker);
-
-                    fetchJsonp(`https://api.vworld.kr/ned/data/ladfrlList?key=${keyVworld}&pnu=${pnu}`)
-                        .then(response => response.json())
-                        .then(data => {
-                            if (!data.ladfrlVOList) return;
-                            const item = data.ladfrlVOList.ladfrlVOList[0];
-                            feature.property_area = Number(item.lndpclAr);
-                            feature.property_jimok = item.lndcgrCodeNm;
-                        })
-                        .catch(error => console.error('Error:', error));
-
-                    fetchJsonp(`https://api.vworld.kr/ned/data/getPossessionAttr?key=${keyVworld}&pnu=${pnu}&numOfRows=1`)
-                        .then(response => response.json())
-                        .then(data => {
-                            if (!data.possessions) return;
-                            const item = data.possessions.field[0];
-                            feature.property_owner = item.posesnSeCodeNm;
-                            feature.property_ownerCount = Number(item.cnrsPsnCo) + 1;
-                        })
-                        .catch(error => console.error('Error:', error));
-
-                    fetchJsonp(`https://api.vworld.kr/ned/data/getLandUseAttr?key=${keyVworld}&pnu=${pnu}&numOfRows=100`)
-                        .then(response => response.json())
-                        .then(data => {
-                            if (!data.landUses) return;
-                            feature.property_landUse = data.landUses.field;
-                        })
-                        .catch(error => console.error('Error:', error));
-
-                    fetchJsonp(`https://api.vworld.kr/ned/data/getIndvdLandPriceAttr?key=${keyVworld}&pnu=${pnu}&numOfRows=100`)
-                        .then(response => response.json())
-                        .then(data => {
-                            if (!data.indvdLandPrices) return;
-                            feature.property_price = data.indvdLandPrices.field.reverse();
-                        })
-                        .catch(error => console.error('Error:', error));
-
-                    setTimeout(() => addFeature(feature), 500);
+                    addFeature(feature);
                 })
-                .catch(error => console.error('Error:', error));
+                .catch(error => console.error(error));
         };
 
         toolRef.current.children[toolRef.current.children.length - 3].addEventListener('change', () => {
@@ -107,7 +90,7 @@ function Map() {
         });
 
         map.addListener('click', selectSite);
-    }, [naver]);
+    }, []);
 
     return (
         <section ref={mapRef} id='Map'>
--- src/elements/Site.js
+++ src/elements/Site.js
@@ -6,6 +6,7 @@ function Site() {
     const { features } = stateFeatures();
     const [totalArea, setTotalArea] = useState(0);
     const [totalJiga, setTotalJiga] = useState(0);
+    const [yearJiga, setYearJiga] = useState(new Date().getFullYear());
     const [unit, setUnit] = useState(1);
     const dataLandUse = [
         { code: 'UQA111', name: '제1종전용주거지역' },
@@ -13,6 +14,7 @@ function Site() {
         { code: 'UQA121', name: '제1종일반주거지역' },
         { code: 'UQA122', name: '제2종일반주거지역' },
         { code: 'UQA123', name: '제3종일반주거지역' },
+        { code: 'UQA130', name: '준주거지역' },
         { code: 'UQA210', name: '중심상업지역' },
         { code: 'UQA220', name: '일반상업지역' },
         { code: 'UQA230', name: '근린상업지역' },
@@ -23,25 +25,40 @@ function Site() {
         { code: 'UQA410', name: '보전녹지지역' },
         { code: 'UQA420', name: '생산녹지지역' },
         { code: 'UQA430', name: '자연녹지지역' },
+        { code: 'UQB100', name: '계획관리지역' },
+        { code: 'UQB200', name: '생산관리지역' },
+        { code: 'UQB300', name: '보전관리지역' },
+        { code: 'UQC001', name: '농림지역' },
+        { code: 'UQD001', name: '자연환경보전지역' },
     ]
 
-    const getLandUse = (features) => {
-        const landUses = [];
-        for (var feature of features) {
-            for (var i of feature.property_landUse)
-                if (i.prposAreaDstrcCode.slice(0, 3) === 'UQA')
-                    if (i.cnflcAt === '1')
-                        for (var j of dataLandUse)
-                            if (j.code.toString() === i.prposAreaDstrcCode.toString())
-                                if (landUses.indexOf(i.prposAreaDstrcCodeNm) < 0) landUses.push(i.prposAreaDstrcCodeNm);
+    const getJiga = (feature, year) => {
+        if (!feature.property_jiga) return;
 
-            for (var ii of feature.property_landUse)
-                if (ii.prposAreaDstrcCode.slice(0, 3) === 'UQA')
-                    if (ii.cnflcAt === '2')
-                        for (var jj of dataLandUse)
-                            if (jj.code.toString() === ii.prposAreaDstrcCode.toString())
-                                if (landUses.indexOf(ii.prposAreaDstrcCodeNm) < 0) landUses.push(ii.prposAreaDstrcCodeNm);
-        }
+        for (var i of feature.property_jiga)
+            if (i.stdrYear === year)
+                return [Number(i.pblntfPclnd), `${i.stdrYear}-${i.stdrMt}`];
+
+        return [0,];
+    };
+
+    const getLandUse = (feature) => {
+        if (!feature.property_landUse) return;
+
+        const landUses = [];
+
+        for (var i of feature.property_landUse)
+            if (i.cnflcAt === '1')
+                for (var j of dataLandUse)
+                    if (j.code.toString() === i.prposAreaDstrcCode.toString())
+                        if (landUses.indexOf(i.prposAreaDstrcCodeNm) < 0) landUses.push(i.prposAreaDstrcCodeNm);
+
+        for (var ii of feature.property_landUse)
+            if (ii.cnflcAt === '2')
+                for (var jj of dataLandUse)
+                    if (jj.code.toString() === ii.prposAreaDstrcCode.toString())
+                        if (landUses.indexOf(ii.prposAreaDstrcCodeNm) < 0) landUses.push(ii.prposAreaDstrcCodeNm);
+
         return landUses.join(' ');
     };
 
@@ -50,13 +67,13 @@ function Site() {
         let jiga = 0;
 
         for (var i of features) {
-            area += i.property_area;
-            jiga += i.property_area * i.property_jiga;
+            area += i.property_parea;
+            jiga += i.property_parea * getJiga(i, yearJiga)[0];
         }
 
         setTotalArea(area);
         setTotalJiga(jiga);
-    }, [features]);
+    }, [features, yearJiga]);
 
     return (
         <section id='Site'>
@@ -65,18 +82,20 @@ function Site() {
                 <p>선택한 대지의</p>
                 <p>
                     개수는
-                    {' '}<span>{`${features.length}`}</span>{' '}
-                    <b>개</b> 이고,
+                    {' '}<span>{`${features.length}`}</span>
+                    <b> 개</b> 이고,
                 </p>
                 <p>
                     면적은
-                    {' '}<span>{`${totalArea === 0 ? 0 : (totalArea * unit).toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}</span>{' '}
-                    <b>{unit === 1 ? '제곱미터' : '평'}</b> 이고,
+                    {' '}<span>{`${totalArea === 0 ? 0 : (totalArea * unit).toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}</span>
+                    <b> {unit === 1 ? '제곱미터' : '평'}</b> 이고,
                 </p>
                 <p>
                     공시가격은
-                    {' '}<span>{`${totalJiga.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}</span>{' '}
-                    <b>원</b> 입니다.
+                    <input type='number' dir='rtl' min={1989} max={new Date().getFullYear()} defaultValue={yearJiga} onChange={(e) => setYearJiga(e.target.value)} />
+                    <b>년</b> 기준
+                    {' '}<span>{`${totalJiga.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`}</span>
+                    <b> 원</b> 입니다.
                 </p>
             </div>
             <h2>LIST</h2>
@@ -84,13 +103,13 @@ function Site() {
                 <div className='item' onClick={() => unit === 1 ? setUnit(.3025) : setUnit(1)}>
                     <small><b>{unit === 1 ? '제곱미터' : '평'}</b></small>
                     <i className='fa-solid fa-arrow-right-arrow-left' />
-                    <small><b>{unit === 1 ? '평' : '제곱미터'}</b></small>
+                    <small><b>{unit !== 1 ? '제곱미터' : '평'}</b></small>
                 </div>
                 <div className='item'
                     onClick={() => {
                         if (features.length === 0) return;
                         let csv = '번호,주소,지목,용도,면적(m²),공시가격,공시일,소유자,소유자수\n';
-                        for (var i in features) csv += `${Number(i) + 1},${features[i].property_addr},${features[i].property_jimok},${getLandUse([features[i]])},${features[i].property_area},${features[i].property_jiga},${features[i].property_price && `${features[i].property_price[0].stdrYear}-${features[i].property_price[0].stdrMt}`},${features[i].property_owner},${features[i].property_ownerCount}\n`;
+                        for (var i in features) csv += `${Number(i) + 1},${features[i].property_addr},${features[i].property_jimok},${getLandUse(features[i])},${features[i].property_parea},${getJiga(features[i], yearJiga)[0]},${getJiga(features[i], yearJiga)[1]},${features[i].property_owner_nm},${features[i].property_owner_count}\n`;
                         const csvFile = new Blob(['\ufeff' + csv], { type: 'text/csv' });
                         const link = document.createElement('a');
                         link.href = window.URL.createObjectURL(csvFile);
@@ -147,22 +166,22 @@ function Site() {
                                 {item.property_jimok}
                             </td>
                             <td>
-                                {getLandUse([item])}
+                                {getLandUse(item)}
                             </td>
                             <td style={{ textAlign: 'right' }}>
-                                {(item.property_area * unit).toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                {(item.property_parea * unit).toFixed(1).replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                             </td>
                             <td style={{ textAlign: 'right' }}>
-                                {item.property_price && (item.property_price[0].pblntfPclnd * unit).toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                {getJiga(item, yearJiga)[0] === 0 ? undefined : (getJiga(item, yearJiga)[0] * unit).toFixed().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                             </td>
                             <td>
-                                {item.property_price && `${item.property_price[0].stdrYear}-${item.property_price[0].stdrMt}`}
+                                {getJiga(item, yearJiga)[1]}
                             </td>
                             <td>
-                                {item.property_owner}
+                                {item.property_owner_nm}
                             </td>
                             <td>
-                                {item.property_ownerCount}
+                                {item.property_owner_count}
                             </td>
                         </tr>
                     ))}
